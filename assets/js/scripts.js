b1cb469b851a1e5bfb766c9501460b734ad2b129
Michael Knowlton <mknowlton89@gmail.com>
2021-03-29 15:56:40 -0400

    Got moment.js wired up and wired up the logic for the uv index background color to update based on the value.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,12 @@
 // DOM Variables
 let cityInputEl = $("#city-input");
 let searchHistoryEl = $("#search-history");
+let today = moment();
 
 
 
 // JS Variables
-let url = "http://api.openweathermap.org/data/2.5/weather";
+let url = "http://api.openweathermap.org/data/2.5/weather?q=";
 const apiKey = "&appid=6606e9501ff48568589dcb47972390e6";
 const units = "&units=imperial"
 let cityKey;
@@ -23,6 +24,20 @@ function lookupUvi(lat, lon) {
         })
         .then(function (data) {
             $('#uv-index-indicator').text(data.current.uvi);
+
+            if (data.current.uvi < 3) {
+                $('#uv-index-indicator').css("background-color", "green");
+            } else if (data.current.uvi >= 3 && data.current.uvi < 6) {
+                $('#uv-index-indicator').css("background-color", "yellow");
+                $('#uv-index-indicator').css("color", "black");
+                // $('#uv-index-indicator').attr("style", "color: black");
+            } else if (data.current.uvi >= 6 && data.current.uvi < 8) {
+                $('#uv-index-indicator').css("background-color", "orange");
+            } else if (data.current.uvi >= 8 && data.current.uvi < 11) {
+                $('#uv-index-indicator').css("background-color", "red");
+            } else if (data.current.uvi >= 11) {
+                $('#uv-index-indicator').css("background-color", "purple");
+            }
         });
 }
 
@@ -36,8 +51,7 @@ function lookupWeather() {
             return response.json();
         })
         .then(function (data) {
-            $('#city-name').text(data.name);
-            // $('#date').text(moment().format(MM DD YYY));
+            $('#city-name').text(data.name + " (" + today.format("l") + ")");
             $('#daily-temp').text(data.main.temp);
             $('#daily-humidity').text(data.main.humidity);
             $('#wind-speed-indicator').text(data.wind.speed);
@@ -52,20 +66,20 @@ function lookupWeather() {
 
 // Event listeners
 $("#input").submit(function (event) {
-    cityInput = cityInputEl.val().toLowerCase();
-    cityKey = ("?q=" + cityInput);
+    cityKey = cityInputEl.val().toLowerCase();
+    // cityKey = ("?q=" + cityInput);
     event.preventDefault();
 
     // Get local storage
     let searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
 
     // Append an li item to the search history list
-    let liEl = $('<li>').text(cityInput);
+    let liEl = $('<li>').text(cityKey);
     liEl.addClass("list-group-item");
     searchHistoryEl.append(liEl);
 
     // Push into it
-    searchHistory.push(cityInput);
+    searchHistory.push(cityKey);
 
     // Stringify and set to local storage
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
